3b8b8d20715357983ba897ec7b9f6fb869b9bb51
Eyal Mesika <seary.realestate@gmail.com>
2025-08-30 21:46:10 +0300

    remove Hebrow remarks

--- test_data_fetch.py
+++ test_data_fetch.py
@@ -2,9 +2,9 @@ from data_fetcher import get_stock_history, get_company_news
 from pprint import pprint
 
 df = get_stock_history("AAPL")
-print(df.tail())  # שורות אחרונות לבדיקה
+print(df.tail())
 
 news = get_company_news("AAPL", "2024-12-01", "2024-12-31")
-pprint(news[:2])  # שתי ידיעות לדוגמה
+pprint(news[:2])
 
 print("=== Done ===")
